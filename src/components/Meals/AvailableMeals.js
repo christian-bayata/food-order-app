1f76cb361fa140c27cdfd9037cbe7af8f26daa0b
christian-bayata <franksagie1@gmail.com>
2024-02-01 13:13:23 +0100

    Add meals component

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,40 @@
+import classes from "./AvailableMeals.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Suya",
+    description: "Spicy smoked grilled barbecue",
+    price: 850.5,
+  },
+  {
+    id: "m2",
+    name: "Jollof Rice With Fried Chicken",
+    description: "A popular spiced rice and tomato-based West African dish",
+    price: 2500.0,
+  },
+  {
+    id: "m3",
+    name: "Egusi Soup with Goat Meat",
+    description: "Nigerian soup delicacy",
+    price: 3450.5,
+  },
+  {
+    id: "m4",
+    name: "Fried Rice With Turkey",
+    description: "A popular spiced rice flavour",
+    price: 2500.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
