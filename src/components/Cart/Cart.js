67ac87cdb043bbecedf435b99f55b397eb905601
christian-bayata <franksagie1@gmail.com>
2024-02-05 13:54:56 +0100

    sligh adjustment in cart item add handler

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} nme={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler /* .bind(null, item.id) */} onAdd={cartItemAddHandler /* (bind, item) */} />
+        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler /* .bind(null, item.id) */} onAdd={cartItemAddHandler /* (bind, item) */} />
       ))}
     </ul>
   );
@@ -27,8 +27,8 @@ const Cart = (props) => {
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
+        <span>Total Amount</span>
         <span>{totalAmount}</span>
-        <span>35.62</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -3,6 +3,8 @@ import classes from "./CartItem.module.css";
 const CartItem = (props) => {
   const price = `₦${props.price.toFixed(2)}`;
 
+  console.log(props);
+
   return (
     <li className={classes["cart-item"]}>
       <div>
